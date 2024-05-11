14d84b5f3f075470965183f6f5c607130ed11367
jinhchoi <dandyday31@gmail.com>
2024-05-10 23:22:58 -0400

    entrypoint modified

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 if [ ! -f "config.php" ]; then
 	wp core download --allow-root
 	wp config create --allow-root --dbname=$MYSQL_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --dbprefix=wp_
-	wp user create --allow-root $MYSQL_NEW_USER $MYSQL_NEW_EMAIL --user_pass=$MYSQL_NEW_PASSWORD
+	wp user create --allow-root $WP_NEW_USER $WP_NEW_EMAIL --user_pass=$WP_NEW_PASSWORD
 	wp core install --allow-root --url="$DOMAIN_NAME" --title="$WP_SITE_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL"
 fi
 
